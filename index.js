8b62beeb48e6496e03fdff420fd88b67f935cb90
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-29 12:10:06 -0700

    added to the markdown function

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
 
-// const Employee = require("./lib/employee.js");
-// const Manager = require("./lib/manager.js");
-// const Intern = require("./lib/intern.js");
-// const Engineer = require("./lib/engineer.js");
+
+const Manager = require("./lib/manager.js");
+
 const inquirer = require("inquirer");
-
-// const qustions = [
-
-//     {
-//         type: "input",
-//         name: "name",
-//         message: "What is your manager's name?"
-//     },
-//     {
-//         type: "input",
-//         name: "id",
-//         message: "What is the team manager's id?"
-//     },
-//     {
-//         type: "input",
-//         name: "officeNumber",
-//         message: "What is the team manager's officenumber?"
-//     },
-//     {
-//         type: "input",
-//         name: "officenumber",
-//         message: "What is the team manager's officenumber?"
-//     },
-//     {
-//         type: "list",
-//         name: "teamMember",
-//         message: "What type of team member would you like to add?",
-//         choices: ["Engineer", "Intern", "I don't want to add anymore team members"],
-//     },
-
-// ];
+const fs = require("fs");
+const generator = require("./generator");
 
 function managerQuestions() {
 inquirer.prompt([
@@ -56,18 +26,12 @@ inquirer.prompt([
    
     ])
 .then((data) => {
-    const managerInfo = 
+    const markdown = generator(data); 
+    fs.writeFile("generator.js", markdown, (err) => {
+            err ? console.log(err) : console.log("File was written!!");
+
 })
-};
-managerQuestions();
-
-
-// {
-//         type: "list",
-//         name: "teamMember",
-//         message: "What type of team member would you like to add?",
-//         choices: ["Engineer", "Intern", "I don't want to add anymore team members"],
-//     },
+});
 
 
 
@@ -80,9 +44,3 @@ managerQuestions();
 
 
 
-//     function init() {
-//         inquirer.prompt(questions).then((data) => {
-//           console.log(data);
-//         })
-//     }
-//     init();
