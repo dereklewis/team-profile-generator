d02c0126a8e97664a86d147c3f9e281af29b9247
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-31 09:01:27 -0600

    added function for the team member question and finished manager info

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generator = require("./generator");
 
+const newEmployees = []
+
 function managerQuestions() {
 inquirer.prompt([
     {
@@ -20,22 +22,44 @@ inquirer.prompt([
     },
     {
         type: "input",
+        name: "email",
+        message: "What is the team manager's email address"
+    },
+
+    {
+        type: "input",
         name: "officeNumber",
         message: "What is the team manager's officenumber?"
     },
    
     ])
 .then((data) => {
-    const markdown = generator(data); 
-    fs.writeFile("generator.js", markdown, (err) => {
-            err ? console.log(err) : console.log("File was written!!");
+    console.log(data);
 
-})
+    const managerOne = new Manager(data.id, data.name, data.email, data.officeNumber);
+    newEmployees.push(managerOne); 
+
+    teamQuestions();
 });
+    
+}
+managerQuestions();
 
-
-
-
+function teamQuestions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "teamMember",
+            message: "Would you like to add a new team member?",
+            choices: ["Intern", "Engineer", "Finish Building Team"],
+        }
+        
+    ])
+    .then((teamAnswer) => {
+        console.log(teamAnswer);
+    })
+    
+}
 
 
 
